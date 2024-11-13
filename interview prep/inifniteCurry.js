aeac52f7f9ced2f905d498049c61e3ff63582861
ShwetaMane13 <shwetamane1397@gmail.com>
2024-11-13 17:26:52 +0530

    infinite curry

--- interview prep/inifniteCurry.js
+++ interview prep/inifniteCurry.js
@@ -12,3 +12,5 @@ let res = add(4);
 // then you call it with 5 it returns add(4+5)
 // then you call it with 6 it returns add(9+6)
 // for the last one the if condition turns out false hence it returns a which is 15
+
+//sum = x => y => y ? sum(x + y) : x;  //arrow function syntax
