9340f84f56c5751fa5ab4e088746d0724b87d57c
ShwetaMane13 <shwetamane1397@gmail.com>
2025-04-25 11:05:24 +0530

    30DOJ: day17 - Map, ForEach and Selection Sort

---
+++ 30DaysOfJavascript/day17.js
@@ -0,0 +1,42 @@
+// Topic: Map, ForEach and Selection Sort
+
+// difference between map and foreach
+// map - returns a new array with the results of calling function on every element
+// forEach - just runs the function on each element but does not return anything
+
+const numbers = [1, 2, 3];
+
+const doubled = numbers.map((num) => num * 2);
+console.log(doubled); // [2, 4, 6]
+
+const doubledAgain = [];
+numbers.forEach((num) => doubledAgain.push(num * 2));
+console.log(doubledAgain); // [2, 4, 6]
+
+// so if you need a new transformed array, go with map
+// if you are doing just something per item (like logging or mutating external stuff), go with forEach
+
+// DSA - Selection Sort
+
+const selectionSort = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+    if (minIndex !== i) {
+      [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+    }
+  }
+
+  return arr;
+};
+
+console.log(selectionSort([5, 3, 8, 4, 2]));
+
+// how it works
+// find the smallest element in the array
+// swap it with the first element
+// repeat the process with the rest of the array (excluding the already sorted part)
